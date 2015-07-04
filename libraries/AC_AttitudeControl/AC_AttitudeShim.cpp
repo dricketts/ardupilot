3d843113d7145a11d37edbe338d88b87f43bcf48
Mario Alvarez <mmcgil2@gmail.com>
2015-07-03 17:27:49 -0700

    Added logic to ensure that attitude setting function is called whenever throttle is set (and vice versa)

---
+++ libraries/AC_AttitudeControl/AC_AttitudeShim.cpp
@@ -0,0 +1,176 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
+
+#include "AC_AttitudeControl.h"
+#include "AC_AttitudeShim.h"
+#include <AP_HAL.h>
+
+// Attitude shim implementation
+
+// Shim entry point
+// Params: stores a description of which attitude/throttle function to call out to
+// first_call: if true, call the throttle controller after making an attitude update (or vice versa)
+// used to prevent unbounded recursion wherein we update attitude and throttle repeatedly forever
+void AC_AttitudeShim::attitude_shim_entry_point(Att_shim_params& params, bool first_call) {
+  // Keep around two old sets of params:
+  // One for most recently called attitude function
+  static Att_shim_params last_attitude_call = {
+    ATT_SET_SMOOTH, // which_fn
+    0.0, 0.0, 0.0, // roll, pitch, yaw
+    0.0, // smoothing_gain
+    false, // slew_yaw
+    0, // throttle
+    false, // angle_boost
+    false // NOT yet initialized
+  };
+
+  // One for most recently called throttle function
+  static Att_shim_params last_throttle_call = {
+    THROTTLE_SET, // which_fn
+    0.0, 0.0, 0.0, // roll, pitch, yaw
+    0.0, // smoothing_gain
+    false, // slew_yaw
+    0, // throttle
+    false, // angle_boost
+    false // NOT yet initialized
+  };
+
+  // Call the function that the user wanted.
+  // Then, if the function was throttle, see if the user previously called
+  // An attitude function (or vice versa). If they did, re-issue that same call.
+  // This is to ensure throttle and attitude are updated at same frequency
+  switch (params.which_fn) {
+    case ATT_SET_SMOOTH :
+      AC_AttitudeControl::angle_ef_roll_pitch_rate_ef_yaw_smooth(params.roll, params.pitch, params.yaw, params.smoothing_gain);
+      if(last_throttle_call.initialized && first_call){
+        AC_AttitudeShim::attitude_shim_entry_point(last_throttle_call, false);
+      }
+      last_attitude_call = params;
+      break;
+
+    case ATT_SET :
+      AC_AttitudeControl::angle_ef_roll_pitch_rate_ef_yaw(params.roll, params.pitch, params.yaw);
+      if(last_throttle_call.initialized && first_call){
+        AC_AttitudeShim::attitude_shim_entry_point(last_throttle_call, false);
+      }
+      last_attitude_call = params;
+      break;
+
+    case ATT_SET_SLEW :
+      AC_AttitudeControl::angle_ef_roll_pitch_yaw(params.roll, params.pitch, params.yaw, params.slew_yaw);
+      if(last_throttle_call.initialized && first_call){
+        AC_AttitudeShim::attitude_shim_entry_point(last_throttle_call, false);
+      }
+      last_attitude_call = params;
+      break;
+
+    case ATT_SET_RATES :
+      AC_AttitudeControl::rate_ef_roll_pitch_yaw(params.roll, params.pitch, params.yaw);
+      if(last_throttle_call.initialized && first_call){
+        AC_AttitudeShim::attitude_shim_entry_point(last_throttle_call, false);
+      }
+      last_attitude_call = params;
+      break;
+
+    case ATT_SET_BODYFRAME:
+      AC_AttitudeControl::rate_bf_roll_pitch_yaw(params.roll, params.pitch, params.yaw);
+      if(last_throttle_call.initialized && first_call){
+        AC_AttitudeShim::attitude_shim_entry_point(last_throttle_call, false);
+      }
+      last_attitude_call = params;
+      break;
+
+    case THROTTLE_SET:
+      AC_AttitudeControl::set_throttle_out(params.throttle, params.angle_boost);
+      if(last_attitude_call.initialized && first_call){
+        AC_AttitudeShim::attitude_shim_entry_point(last_throttle_call, false);
+      }
+      last_throttle_call = params;
+      break;
+  }
+}
+
+//
+// Overrides (call into entry point)
+//
+
+// angle_ef_roll_pitch_rate_ef_yaw_smooth - attempts to maintain a roll and pitch angle and yaw rate (all earth frame) while smoothing the attitude based on the feel parameter
+//      smoothing_gain : a number from 1 to 50 with 1 being sluggish and 50 being very crisp
+void AC_AttitudeShim::angle_ef_roll_pitch_rate_ef_yaw_smooth(float roll_angle_ef, float pitch_angle_ef, float yaw_rate_ef, float smoothing_gain) {
+  Att_shim_params params;
+
+  params.which_fn = ATT_SET_SMOOTH;
+  params.roll = roll_angle_ef;
+  params.pitch = pitch_angle_ef;
+  params.yaw = yaw_rate_ef;
+  params.smoothing_gain = smoothing_gain;
+  params.initialized = true;
+
+  AC_AttitudeShim::attitude_shim_entry_point(params, true);
+}
+
+// angle_ef_roll_pitch_rate_ef_yaw - attempts to maintain a roll and pitch angle and yaw rate (all earth frame)
+void AC_AttitudeShim::angle_ef_roll_pitch_rate_ef_yaw(float roll_angle_ef, float pitch_angle_ef, float yaw_rate_ef) {
+  Att_shim_params params;
+
+  params.which_fn = ATT_SET;
+  params.roll = roll_angle_ef;
+  params.pitch = pitch_angle_ef;
+  params.yaw = yaw_rate_ef;
+  params.initialized = true;
+
+  AC_AttitudeShim::attitude_shim_entry_point(params, true);
+}
+
+// angle_ef_roll_pitch_yaw - attempts to maintain a roll, pitch and yaw angle (all earth frame)
+//  if yaw_slew is true then target yaw movement will be gradually moved to the new target based on the YAW_SLEW parameter
+void AC_AttitudeShim::angle_ef_roll_pitch_yaw(float roll_angle_ef, float pitch_angle_ef, float yaw_angle_ef, bool slew_yaw){
+  Att_shim_params params;
+
+  params.which_fn = ATT_SET_SLEW;
+  params.roll = roll_angle_ef;
+  params.pitch = pitch_angle_ef;
+  params.yaw = yaw_angle_ef;
+  params.slew_yaw = slew_yaw;
+  params.initialized = true;
+
+  AC_AttitudeShim::attitude_shim_entry_point(params, true);
+}
+
+// rate_ef_roll_pitch_yaw - attempts to maintain a roll, pitch and yaw rate (all earth frame)
+void AC_AttitudeShim::rate_ef_roll_pitch_yaw(float roll_rate_ef, float pitch_rate_ef, float yaw_rate_ef){
+  Att_shim_params params;
+
+  params.which_fn = ATT_SET_RATES;
+  params.roll = roll_rate_ef;
+  params.pitch = pitch_rate_ef;
+  params.yaw = yaw_rate_ef;
+  params.initialized = true;
+
+  AC_AttitudeShim::attitude_shim_entry_point(params, true);
+}
+
+// rate_bf_roll_pitch_yaw - attempts to maintain a roll, pitch and yaw rate (all body frame)
+void AC_AttitudeShim::rate_bf_roll_pitch_yaw(float roll_rate_bf, float pitch_rate_bf, float yaw_rate_bf){
+  Att_shim_params params;
+
+  params.which_fn = ATT_SET_BODYFRAME;
+  params.roll = roll_rate_bf;
+  params.pitch = pitch_rate_bf;
+  params.yaw = yaw_rate_bf;
+  params.initialized = true;
+
+  AC_AttitudeShim::attitude_shim_entry_point(params, true);
+}
+
+// set_throttle_out - to be called by upper throttle controllers when they wish to provide throttle output directly to motors
+// provide 0 to cut motors
+void AC_AttitudeShim::set_throttle_out(int16_t throttle_pwm, bool apply_angle_boost){
+  Att_shim_params params;
+
+  params.which_fn = THROTTLE_SET;
+  params.throttle = throttle_pwm;
+  params.angle_boost = apply_angle_boost;
+  params.initialized = true;
+
+  AC_AttitudeShim::attitude_shim_entry_point(params, true);
+}
---
+++ libraries/AC_AttitudeControl/AC_AttitudeShim.h
@@ -0,0 +1,105 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: t -*-
+
+/// @file    AC_AttitudeControl.h
+/// @brief   ArduCopter attitude control library
+
+#ifndef AC_AttitudeShim_H
+#define AC_AttitudeShim_H
+
+#include <AP_Common.h>
+#include <AP_Param.h>
+#include <AP_Math.h>
+#include <AP_InertialSensor.h>
+#include <AP_AHRS.h>
+#include <AP_Motors.h>
+#include <DataFlash.h>
+#include <AC_PID.h>
+#include <AC_P.h>
+
+// This implements a shim over the quadcopter attitude controller
+// Enabling run-time monitoring at the level of abstraction of sending
+// Throttle/attitude commands.
+
+// TODO: Have a shim stats module
+
+// Enumeration of the functions we shim over (used for dispatching)
+enum Att_shim_fn {ATT_SET_SMOOTH, ATT_SET, ATT_SET_SLEW, ATT_SET_RATES, ATT_SET_BODYFRAME, THROTTLE_SET};
+
+// Data structure used for passing parameters to shim functions
+// This is NOT the parameters data structure for MAVLink messaging.
+
+struct Att_shim_params {
+  Att_shim_fn which_fn;
+  // pitch, roll, yaw parameters - have different semantics depending on which function
+  float roll;
+  float pitch;
+  float yaw;
+  // extra options for attitude
+  float smoothing_gain;
+  bool slew_yaw;
+  // throttle parameters
+  int16_t throttle;
+  bool angle_boost;
+  // used when storing parameters in static variables
+  bool initialized;
+};
+
+// @class     AC_AttitudeShim
+class AC_AttitudeShim : public AC_AttitudeControl {
+public:
+  AC_AttitudeShim( AP_AHRS &ahrs,
+                        const AP_Vehicle::MultiCopter &aparm,
+                        AP_Motors& motors,
+                        AC_P& pi_angle_roll, AC_P& pi_angle_pitch, AC_P& pi_angle_yaw,
+                        AC_PID& pid_rate_roll, AC_PID& pid_rate_pitch, AC_PID& pid_rate_yaw
+                        ) :
+                        AC_AttitudeControl(ahrs, aparm, motors, pi_angle_roll, pi_angle_pitch,
+                                           pi_angle_yaw, pid_rate_roll, pid_rate_pitch, pid_rate_yaw)
+    {
+    }
+
+    // empty destructor to suppress compiler warning
+    virtual ~AC_AttitudeShim() {}
+
+    //
+    // Methods to be called by upper controllers to request and implement desired attitude
+    // (Overrides)
+    //
+
+    // angle_ef_roll_pitch_rate_ef_yaw_smooth - attempts to maintain a roll and pitch angle and yaw rate (all earth frame) while smoothing the attitude based on the feel parameter
+    //      smoothing_gain : a number from 1 to 50 with 1 being sluggish and 50 being very crisp
+    void angle_ef_roll_pitch_rate_ef_yaw_smooth(float roll_angle_ef, float pitch_angle_ef, float yaw_rate_ef, float smoothing_gain);
+
+    // angle_ef_roll_pitch_rate_ef_yaw - attempts to maintain a roll and pitch angle and yaw rate (all earth frame)
+    void angle_ef_roll_pitch_rate_ef_yaw(float roll_angle_ef, float pitch_angle_ef, float yaw_rate_ef);
+
+    // angle_ef_roll_pitch_yaw - attempts to maintain a roll, pitch and yaw angle (all earth frame)
+    //  if yaw_slew is true then target yaw movement will be gradually moved to the new target based on the YAW_SLEW parameter
+    void angle_ef_roll_pitch_yaw(float roll_angle_ef, float pitch_angle_ef, float yaw_angle_ef, bool slew_yaw);
+
+    // rate_ef_roll_pitch_yaw - attempts to maintain a roll, pitch and yaw rate (all earth frame)
+    void rate_ef_roll_pitch_yaw(float roll_rate_ef, float pitch_rate_ef, float yaw_rate_ef);
+
+    // rate_bf_roll_pitch_yaw - attempts to maintain a roll, pitch and yaw rate (all body frame)
+    void rate_bf_roll_pitch_yaw(float roll_rate_bf, float pitch_rate_bf, float yaw_rate_bf);
+
+    //
+    // throttle functions
+    // (Overrides)
+    //
+
+    // set_throttle_out - to be called by upper throttle controllers when they wish to provide throttle output directly to motors
+    // provide 0 to cut motors
+    void set_throttle_out(int16_t throttle_pwm, bool apply_angle_boost);
+
+private:
+
+  // Main entry point for the shim. All the overriden throttle and attitude functions
+  // Call into this one.
+  // Params stores information about the function to call, and its arguments
+  // first_call stores whether we should also update the throttle if we are
+  // currently updating attitude (or vice versa)
+  void attitude_shim_entry_point(Att_shim_params& params, bool first_call);
+};
+
+#endif //AC_AttitudeShim_H
